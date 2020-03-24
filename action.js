954284f6145b4feba29f37b1e1f0948432be90f4
alexerhardt <alex.erhardt.es@gmail.com>
2020-03-24 09:02:26 +0100

    do count on recovered

--- action.js
+++ action.js
@@ -35,8 +35,8 @@ function extract(filename) {
 
 const [confirmed, dates] = extract(FILENAME_CONFIRMED);
 const [deaths] = extract(FILENAME_DEATHS);
-// const [recovered] = extract(FILENAME_RECOVERED);
-const countries = Object.keys(confirmed);
+const [recovered] = extract(FILENAME_RECOVERED);
+const countries = Object.keys(recovered);
 const results = {};
 countries.forEach(country => {
   results[country] = dates.map(date => {
@@ -45,7 +45,7 @@ countries.forEach(country => {
       date: `2020-${month}-${day}`,
       confirmed: confirmed[country][date],
       deaths: deaths[country][date],
-      // recovered: recovered[country][date]
+      recovered: recovered[country][date]
     };
   });
 });
